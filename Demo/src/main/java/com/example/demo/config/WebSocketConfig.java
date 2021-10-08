cb0d844bd111cb3db590ab3e03a3b7e79dabd2a5
hypvn_partner07@hyperion.co.jp <hyperion-mec>
2021-10-08 07:06:37 +0700

    update-1

--- Demo/src/main/java/com/example/demo/config/WebSocketConfig.java
+++ Demo/src/main/java/com/example/demo/config/WebSocketConfig.java
@@ -1,29 +1,16 @@
 package com.example.demo.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-
-import com.example.demo.interceptor.HttpHandshakeInterceptor;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer  {
 
-	 	@Autowired
-	    private HttpHandshakeInterceptor handshakeInterceptor;
-
-	    @Override
-	    public void registerStompEndpoints(StompEndpointRegistry registry) {
-	        registry.addEndpoint("/ws").withSockJS().setInterceptors(handshakeInterceptor);
-	    }
-
-	    @Override
-	    public void configureMessageBroker(MessageBrokerRegistry registry) {
-	        registry.setApplicationDestinationPrefixes("/app");
-	        registry.enableSimpleBroker("/topic");
-	    }
+	@Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+    registry.addHandler(new SocketHandler(), "/videochat").setAllowedOrigins("*");
+    }
 }
